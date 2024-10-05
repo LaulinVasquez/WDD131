a738640b797bff8cb8eb51841a9c85a0ca5a591c
LaulinVasquez <laurin171125@gmail.com>
2024-10-05 16:10:03 -0600

    update to menu, and cool pics page

---
+++ week 3/coolpics/coolpics.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-btn').addEventListener('click', function() {
+    const nav = document.getElementById('nav-links');
+    nav.classList.toggle('show'); // Toggle the "show" class to display or hide the nav
+  });
